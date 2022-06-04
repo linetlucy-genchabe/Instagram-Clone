d97d05d05b6c0c1dbb00dafcae6e3e956907d9b8
linetlucy-genchabe <linetlucy21@gmail.com>
2022-06-04 11:05:14 +0300

    ft:added profile model

--- lynneinsta/models.py
+++ lynneinsta/models.py
@@ -15,17 +15,17 @@ class Pic(models.Model):
     def __str__(self):
         return self.pic_name
 
-    def delete_pic(self):
-    	self.delete()
-
     def save_pic(self):
     	self.save()
+
+    def delete_pic(self):
+        self.delete()
 
     def update_caption(self,new_caption):
         self.pic_caption = new_caption
         self.save()
         
-        @classmethod
+    @classmethod
     def get_pics_by_user(cls,id):
         sent_pics = Pic.objects.filter(user_id=id)
         return sent_pics
@@ -38,12 +38,33 @@ class Pic(models.Model):
     class Meta:
     	ordering = ['-pub_date']
 
+    def save_profile(self):
+        self.save()
 
     def __str__(self):
     	return self.user.username
 
-    def save_profile(self):
-    	self.save()
+    
     
     
 
+class Profile(models.Model):
+	username = models.CharField(default='User',max_length=30)
+	profile_pic = models.ImageField(upload_to = "profile/",null=True)
+	bio = models.TextField(default='',blank = True)
+	first_name = models.CharField(max_length =30)
+	last_name = models.CharField(max_length =30)
+
+	def __str__(self):
+		return self.username
+
+	def delete_profile(self):
+		self.delete()
+
+	def save_profile(self):
+		self.save()
+
+	@classmethod
+	def search_profile(cls,search_term):
+		got_profiles = cls.objects.filter(first_name__icontains = search_term)
+		return got_profiles
