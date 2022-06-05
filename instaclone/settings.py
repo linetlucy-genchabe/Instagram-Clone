bc08000f33cee543ac359b650b0f2cabf982171b
linetlucy-genchabe <linetlucy21@gmail.com>
2022-06-05 11:36:11 +0300

    added installed apps

--- instaclone/settings.py
+++ instaclone/settings.py
@@ -41,6 +41,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'lynneinsta.apps.LynneinstaConfig',
     'bootstrap5',
+    'tinymce',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
